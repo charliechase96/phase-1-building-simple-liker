adef7c914fd41041605aed4fff28d5387d4435ad
Charlie Gray <charliechasegray@gmail.com>
2023-02-21 13:58:10 -0700

    complete lab

--- main.js
+++ main.js
@@ -3,7 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector("#modal");
+const message = document.querySelector("#modal-message");
 
+let likes = document.querySelectorAll(".like-glyph");
+
+for (let emptyHeart of likes) {
+  emptyHeart.addEventListener("click", addLike)
+}
+
+function addLike(event) {
+  if (event.target.textContent === EMPTY_HEART) {
+    mimicServerCall()
+    .then(function (response) {
+      event.target.textContent = FULL_HEART;
+      event.target.classList.toggle("activated-heart");
+    })
+    .catch(function (error) {
+      modal.classList.toggle("hidden");
+      message.textContent = error;
+      setTimeout(function () {
+        modal.classList.toggle("hidden")
+      }, 3000);
+    })
+  }
+  if (event.target.textContent === FULL_HEART) {
+    event.target.textContent = EMPTY_HEART;
+    event.target.classList.toggle("activated-heart")
+  }
+}
 
 
 
